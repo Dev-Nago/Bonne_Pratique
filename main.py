d26a02825a51e40c4690937c7bdcd44296ad20ab
Dev-Nago <pro.plaiajc@gmail.com>
2024-09-13 10:11:48 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from module import outils
+
+outils.add(5,6)
+outils.sub(12,5)
+outils.multiply(150,3)
+outils.divide(100,2)
---
+++ module/outils.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    print(a+b)
+    return a + b
+
+def sub(a,b):
+    print(a-b)
+    return a-b
+
+def multiply(a,b):
+    print(a*b)
+    return a*b
+
+def divide(a,b):
+    print(a/b)
+    return a/b
